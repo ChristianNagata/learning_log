c5a6be950c865082555b9c81937667090493f339
Christian <christian_nagata@hotmail.com>
2021-10-14 19:52:03 -0300

    Adição de 'get_object_or_404' na função topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Topic
 from django.http import HttpResponseRedirect, Http404
 from .forms import TopicForm, EntryForm
@@ -23,7 +23,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Mostra um único assunto e todas as suas entradas."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
 
     # Garante que o assunto pertence ao usuário atual
     if topic.owner != request.user:
